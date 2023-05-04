20205315bd857dc3f84658716e108e05105ff0f4
Guy <guysayfan@gmail.com>
2023-05-03 19:03:19 -0700

    WIP BoardGenerator

---
+++ Connect4AI/BoardGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect4AI
+{
+    internal class BoardGenerator
+    {
+    }
+}
